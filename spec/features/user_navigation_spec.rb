5c15494517b7fcd34daf41d00d00ef5d50cc9087
Ben Stafford <bendstafford@gmail.com>
2016-05-03 21:44:37 -0400

    Add link to view Sniffers (followers)

---
+++ spec/features/user_navigation_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature "Navigation" do
+  before do
+    @user = FactoryGirl.create(:user)
+    @toot = FactoryGirl.create(:toot, user: @user)
+    @other_user = FactoryGirl.create(:user, first_name: "Follower")
+    @other_toot = FactoryGirl.create(:toot, user: @other_user)
+    sign_in(@user)
+  end
+
+  scenario "should have a link to view Sniffers (followers)" do
+    visit user_path(@user)
+    @user.followers << @other_user 
+    
+    click_link "Sniffers"
+
+    expect(page).to have_css('.followers li', text: @other_user.first_name)
+  end
+end
