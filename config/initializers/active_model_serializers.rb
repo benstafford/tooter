9b4612c075c93e0599aa16523bdd2bdc704ea1fb
Ben Stafford <bendstafford@gmail.com>
2016-05-08 15:51:23 -0400

    Attempt to adjust mime types so that parameters get wrapped correctly

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,1 +1,10 @@
 ActiveModelSerializers.config.adapter = ActiveModelSerializers::Adapter::JsonApi
+
+api_mime_types = %W(
+  application/vnd.api+json
+  text/x-json
+  application/json
+)
+
+Mime::Type.unregister :json
+Mime::Type.register 'application/json', :json, api_mime_types
